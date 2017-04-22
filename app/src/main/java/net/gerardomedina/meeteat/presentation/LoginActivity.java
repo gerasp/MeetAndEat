1bfdbd866ea4808444d791aa716dc93c6d51fd7e
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-22 13:35:40 +0100

    Login activity and launcher icon

--- app/src/main/java/net/gerardomedina/meeteat/presentation/LoginActivity.java
+++ app/src/main/java/net/gerardomedina/meeteat/presentation/LoginActivity.java
@@ -1,4 +1,4 @@
-package net.gerardomedina.meeteat;
+package net.gerardomedina.meeteat.presentation;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -28,6 +28,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import net.gerardomedina.meeteat.R;
 
 import java.util.ArrayList;
 import java.util.List;
