9bc129b26f91bfbec9b0b44c7b662a884ebc86c7
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-23 09:01:33 +0100

    Navigation menu

--- app/src/main/java/net/gerardomedina/meetandeat/view/BaseDrawerActivity.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/BaseDrawerActivity.java
@@ -3,9 +3,7 @@ package net.gerardomedina.meetandeat.view;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.NavigationView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import net.gerardomedina.meetandeat.R;
@@ -20,41 +18,18 @@ public abstract class BaseDrawerActivity extends BaseActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    toolbar.setTitle(R.string.title_home);
-                    return true;
                 case R.id.navigation_dashboard:
-                    toolbar.setTitle(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    toolbar.setTitle(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_contacts:
+                    return true;
+                case R.id.navigation_preferences:
                     return true;
             }
             return false;
         }
     };
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
