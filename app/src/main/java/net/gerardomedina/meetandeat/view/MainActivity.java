8243bb7e9d00fe4c4300a9ba3d539c90eb2981bf
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-23 08:11:42 +0100

    Activity layout structured

--- app/src/main/java/net/gerardomedina/meetandeat/view/MainActivity.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/MainActivity.java
@@ -1,16 +1,7 @@
 package net.gerardomedina.meetandeat.view;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.ViewStub;
 
 import net.gerardomedina.meetandeat.R;
 
@@ -18,16 +9,11 @@ public class MainActivity extends BaseDrawerActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_base_drawer);
+        ViewStub stub = (ViewStub) findViewById(R.id.content_container);
+        stub.setLayoutResource(R.layout.activity_main);
+        stub.inflate();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         super.onCreate(savedInstanceState);
 
