0887b6ab8efab093631271be4d985d738fadf996
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-23 16:08:44 +0100

    Place picker

--- app/src/main/java/net/gerardomedina/meetandeat/view/fragment/DashboardFragment.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/fragment/DashboardFragment.java
@@ -1,13 +1,29 @@
 package net.gerardomedina.meetandeat.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
 import net.gerardomedina.meetandeat.R;
 
+import static android.app.Activity.RESULT_OK;
+
 public class DashboardFragment extends BaseFragment {
+
+
+    private int PLACE_PICKER_REQUEST;
+
     public DashboardFragment() {
     }
 
@@ -17,4 +33,31 @@ public class DashboardFragment extends BaseFragment {
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FloatingActionButton newMeetingButton = (FloatingActionButton) getActivity().findViewById(R.id.newMeetingButton);
+        newMeetingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place selectedPlace = PlacePicker.getPlace(data,getActivity());
+                // Do something with the place
+            }
+        }
+    }
 }
