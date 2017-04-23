7bf7ee5873e809c94cc15a9060daf8ef7f7c2628
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-23 18:13:06 +0100

    Navigation and listview

--- app/src/main/java/net/gerardomedina/meetandeat/view/fragment/DashboardFragment.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/fragment/DashboardFragment.java
@@ -6,13 +6,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
 import net.gerardomedina.meetandeat.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -20,6 +26,7 @@ public class DashboardFragment extends BaseFragment {
 
 
     private int PLACE_PICKER_REQUEST = 1;
+    private ListView meetings;
 
     public DashboardFragment() {
     }
@@ -28,22 +35,38 @@ public class DashboardFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-
         FloatingActionButton newMeetingButton = (FloatingActionButton) view.findViewById(R.id.newMeetingButton);
-        newMeetingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-                try {
-                    startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST);
-                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-
+        populateDashboard(view);
         return view;
+    }
+
+    private void populateDashboard(View view) {
+        ListView resultsListView = (ListView) view.findViewById(R.id.meetings);
+
+        HashMap<String, String> meetings = new HashMap<>();
+        meetings.put("Diana", "3214 Broadway Avenue");
+        meetings.put("Tyga", "343 Rack City Drive");
+        meetings.put("Rich Homie Quan", "111 Everything Gold Way");
+        meetings.put("Donna", "789 Escort St");
+        meetings.put("Bartholomew", "332 Dunkin St");
+        meetings.put("Eden", "421 Angelic Blvd");
+
+        List<HashMap<String, String>> listItems = new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(), listItems, R.layout.fragment_dashboard_item,
+                new String[]{"First Line", "Second Line"},
+                new int[]{R.id.meeting_label, R.id.meeting_date});
+
+
+        for (Object o : meetings.entrySet()) {
+            HashMap<String, String> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry) o;
+            resultsMap.put("First Line", pair.getKey().toString());
+            resultsMap.put("Second Line", pair.getValue().toString());
+            listItems.add(resultsMap);
+        }
+        resultsListView.setAdapter(adapter);
+
+
     }
 
     @Override
@@ -56,7 +79,7 @@ public class DashboardFragment extends BaseFragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
-                Place selectedPlace = PlacePicker.getPlace(data,getActivity());
+                Place selectedPlace = PlacePicker.getPlace(data, getActivity());
                 // Do something with the place
             }
         }
