5aba318c8a225ba3fb05712e84c5179d14a89483
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-26 13:12:50 +0100

    New Meeting Dialog

--- app/src/main/java/net/gerardomedina/meetandeat/view/activity/NewMeetingActivity.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/activity/NewMeetingActivity.java
@@ -1,16 +1,13 @@
 package net.gerardomedina.meetandeat.view.activity;
 
-import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -19,22 +16,31 @@ import com.google.android.gms.location.places.ui.PlacePicker;
 
 import net.gerardomedina.meetandeat.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class NewMeetingActivity extends AppCompatActivity {
 
     public TextView location;
+    private TextView datetime;
     public static final int PLACE_PICKER_REQUEST = 1;
+    private TextView title;
+    private Toolbar toolbar;
+    private Calendar myCalendar;
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newmeeting);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(getString(R.string.new_meeting));
+        setSupportActionBar(toolbar);
 
-        TextView title = (TextView)findViewById(R.id.newMeetingTitleInput);
+        title = (TextView) findViewById(R.id.newMeetingTitleInput);
 
-        location = (TextView)findViewById(R.id.newMeetingLocationInput);
+        location = (TextView) findViewById(R.id.newMeetingLocationInput);
         location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,13 +48,51 @@ public class NewMeetingActivity extends AppCompatActivity {
                 Intent intent;
                 try {
                     intent = builder.build(getParent());
-                    startActivityForResult(intent,PLACE_PICKER_REQUEST);
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
                 } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
-                    Log.e("Google Play",e.getMessage());
+                    Log.e("Google Play", e.getMessage());
                 }
             }
         });
 
+//        myCalendar = Calendar.getInstance();
+//        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int monthOfYear,
+//                                  int dayOfMonth) {
+//                myCalendar.set(Calendar.YEAR, year);
+//                myCalendar.set(Calendar.MONTH, monthOfYear);
+//                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+//                updateLabel();
+//            }
+//        };
+//        datetime = (TextView) findViewById(R.id.newMeetingDatetimeInput);
+//        datetime.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                new DatePickerDialog(getParent(),
+//                        date,
+//                        myCalendar.get(Calendar.YEAR),
+//                        myCalendar.get(Calendar.MONTH),
+//                        5)
+//                        .show();
+//            }
+//        });
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+                newDate.set(year, monthOfYear, dayOfMonth);
+                datetime.setText(dateFormatter.format(newDate.getTime()));
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
 
     }
+//    private void updateLabel() {
+//        String myFormat = "MM/dd/yy";
+//        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+//        datetime.setText(sdf.format(myCalendar.getTime()));
+//    }
 }
