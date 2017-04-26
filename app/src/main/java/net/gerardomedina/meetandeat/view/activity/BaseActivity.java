a8f9147f63da9489ab26b4d954c1b044a0bad57b
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-26 14:14:25 +0100

    New Meeting Fragment

--- app/src/main/java/net/gerardomedina/meetandeat/view/activity/BaseActivity.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/activity/BaseActivity.java
@@ -1,12 +1,21 @@
 package net.gerardomedina.meetandeat.view.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.widget.Toast;
 
 import net.gerardomedina.meetandeat.R;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     public void changeToActivity(Class activity) {
         this.startActivity(new Intent(this, activity));
