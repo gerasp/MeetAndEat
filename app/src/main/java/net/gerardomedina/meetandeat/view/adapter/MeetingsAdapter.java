eb0dbc41085bd6f6b467c3b517056a5745a8462b
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-28 10:34:22 +0100

    Meetings persistence

--- app/src/main/java/net/gerardomedina/meetandeat/view/adapter/MeetingsAdapter.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/adapter/MeetingsAdapter.java
@@ -2,13 +2,14 @@ package net.gerardomedina.meetandeat.view.adapter;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListAdapter;
+import android.widget.TextView;
 
-import net.gerardomedina.meetandeat.view.fragment.DashboardFragment;
+import net.gerardomedina.meetandeat.R;
+import net.gerardomedina.meetandeat.persistence.local.MeetingValues;
 
 public class MeetingsAdapter extends CursorAdapter {
 
@@ -19,11 +20,21 @@ public class MeetingsAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.fragment_dashboard_item, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        TextView meetingTitle = (TextView) view.findViewById(R.id.meeting_title);
+        TextView meetingDateTime = (TextView) view.findViewById(R.id.meeting_datetime);
+        View meetingColor = view.findViewById(R.id.meeting_color);
 
+        String title = cursor.getString(cursor.getColumnIndexOrThrow(MeetingValues.COLUMN_NAME_TITLE));
+        String datetime = cursor.getString(cursor.getColumnIndexOrThrow(MeetingValues.COLUMN_NAME_DATE))
+                +" "+cursor.getString(cursor.getColumnIndexOrThrow(MeetingValues.COLUMN_NAME_TIME));
+        String color = (cursor.getString(cursor.getColumnIndexOrThrow(MeetingValues.COLUMN_NAME_COLOR)));
+        meetingTitle.setText(title);
+        meetingDateTime.setText(datetime);
+        meetingColor.setBackgroundColor(Integer.valueOf(color, 16));
     }
 }
