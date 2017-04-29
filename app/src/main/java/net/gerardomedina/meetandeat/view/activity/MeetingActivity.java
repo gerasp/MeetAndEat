c1ac6f54ac720d53a4ffb846b8a4a4bb2864d0c5
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-29 21:08:32 +0100

    Floating buttons

--- app/src/main/java/net/gerardomedina/meetandeat/view/activity/MeetingActivity.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/activity/MeetingActivity.java
@@ -76,39 +76,39 @@ public class MeetingActivity extends BaseActivity {
             }
         });
 
-        final Button addFoodButton = (Button) findViewById(R.id.add_food_button);
-        addFoodButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final AddFoodDialog addFoodDialog = new AddFoodDialog(getActivity());
-                addFoodDialog.setView(getActivity().getLayoutInflater().inflate(R.layout.dialog_addfood, null));
-                addFoodDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(android.R.string.ok), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                });
-                addFoodDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        addFoodDialog.dismiss();
-                    }
-                });
-                addFoodDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-                    @Override
-                    public void onShow(DialogInterface dialog) {
-                        Button b = addFoodDialog.getButton(AlertDialog.BUTTON_POSITIVE);
-                        b.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                addFoodDialog.attemptAddFood();
-                            }
-                        });
-                    }
-                });
-
-                addFoodDialog.show();
-            }
-        });
+//        final Button addFoodButton = (Button) findViewById(R.id.add_food_button);
+//        addFoodButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                final AddFoodDialog addFoodDialog = new AddFoodDialog(getActivity());
+//                addFoodDialog.setView(getActivity().getLayoutInflater().inflate(R.layout.dialog_addfood, null));
+//                addFoodDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                    }
+//                });
+//                addFoodDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        addFoodDialog.dismiss();
+//                    }
+//                });
+//                addFoodDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+//                    @Override
+//                    public void onShow(DialogInterface dialog) {
+//                        Button b = addFoodDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+//                        b.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                addFoodDialog.attemptAddFood();
+//                            }
+//                        });
+//                    }
+//                });
+//
+//                addFoodDialog.show();
+//            }
+//        });
 
         if (appCommon.hasInternet(this)) new GetFoodTask(this).execute();
         else {
