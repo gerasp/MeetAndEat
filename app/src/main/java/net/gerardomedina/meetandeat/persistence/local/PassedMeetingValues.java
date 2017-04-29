f35305d3933ccf6ffaac01e18fc67f2ff8e645ee
Gerardo Medina <gera.mdlh@gmail.com>
2017-04-29 14:58:55 +0100

    History

---
+++ app/src/main/java/net/gerardomedina/meetandeat/persistence/local/PassedMeetingValues.java
@@ -0,0 +1,7 @@
+package net.gerardomedina.meetandeat.persistence.local;
+
+import android.provider.BaseColumns;
+
+public class PassedMeetingValues extends MeetingValues implements BaseColumns {
+    public static final String TABLE_NAME = "passedMeeting";
+}
--- app/src/main/java/net/gerardomedina/meetandeat/view/fragment/HistoryFragment.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/fragment/HistoryFragment.java
@@ -13,7 +13,7 @@ import android.widget.ListView;
 import net.gerardomedina.meetandeat.R;
 import net.gerardomedina.meetandeat.common.Meeting;
 import net.gerardomedina.meetandeat.persistence.local.DBHelper;
-import net.gerardomedina.meetandeat.persistence.local.MeetingValues;
+import net.gerardomedina.meetandeat.persistence.local.PassedMeetingValues;
 import net.gerardomedina.meetandeat.task.GetMeetingsTask;
 import net.gerardomedina.meetandeat.view.activity.BaseActivity;
 import net.gerardomedina.meetandeat.view.activity.NewMeetingActivity;
@@ -34,7 +34,7 @@ public class HistoryFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        view = inflater.inflate(R.layout.fragment_history, container, false);
 
         FloatingActionButton newMeetingButton = (FloatingActionButton) view.findViewById(R.id.newMeetingButton);
         newMeetingButton.setOnClickListener(new View.OnClickListener() {
@@ -56,16 +56,16 @@ public class HistoryFragment extends BaseFragment {
     public void saveMeetingListToLocalDB(JSONObject response) throws JSONException {
         JSONArray results = response.getJSONArray("results");
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        db.delete(MeetingValues.TABLE_NAME, null, null);
+        db.delete(PassedMeetingValues.TABLE_NAME, null, null);
         ContentValues values = new ContentValues();
         for (int i = 0; i < results.length(); i++) {
-            values.put(MeetingValues._ID, results.getJSONObject(i).getInt("id"));
-            values.put(MeetingValues.COLUMN_NAME_TITLE, results.getJSONObject(i).getString("title"));
-            values.put(MeetingValues.COLUMN_NAME_LOCATION, results.getJSONObject(i).getString("location"));
-            values.put(MeetingValues.COLUMN_NAME_DATE, results.getJSONObject(i).getString("date"));
-            values.put(MeetingValues.COLUMN_NAME_TIME, results.getJSONObject(i).getString("time"));
-            values.put(MeetingValues.COLUMN_NAME_COLOR, results.getJSONObject(i).getString("color"));
-            db.insert(MeetingValues.TABLE_NAME, null, values);
+            values.put(PassedMeetingValues._ID, results.getJSONObject(i).getInt("id"));
+            values.put(PassedMeetingValues.COLUMN_NAME_TITLE, results.getJSONObject(i).getString("title"));
+            values.put(PassedMeetingValues.COLUMN_NAME_LOCATION, results.getJSONObject(i).getString("location"));
+            values.put(PassedMeetingValues.COLUMN_NAME_DATE, results.getJSONObject(i).getString("date"));
+            values.put(PassedMeetingValues.COLUMN_NAME_TIME, results.getJSONObject(i).getString("time"));
+            values.put(PassedMeetingValues.COLUMN_NAME_COLOR, results.getJSONObject(i).getString("color"));
+            db.insert(PassedMeetingValues.TABLE_NAME, null, values);
         }
         loadMeetingListFromLocalDB();
 
@@ -73,12 +73,12 @@ public class HistoryFragment extends BaseFragment {
 
     private void loadMeetingListFromLocalDB() {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Cursor cursor = db.rawQuery("select * from "+MeetingValues.TABLE_NAME+
-                " order by " +MeetingValues.COLUMN_NAME_DATE+","+MeetingValues.COLUMN_NAME_TIME+ " ASC;"
+        Cursor cursor = db.rawQuery("select * from "+PassedMeetingValues.TABLE_NAME+
+                " order by " +PassedMeetingValues.COLUMN_NAME_DATE+","+PassedMeetingValues.COLUMN_NAME_TIME+ " ASC;"
                 ,null);
         if (cursor.getCount()>0 && cursor.moveToFirst()) appCommon.setNextMeeting(new Meeting(1,"","",
-                cursor.getString(cursor.getColumnIndexOrThrow(MeetingValues.COLUMN_NAME_DATE)),
-                cursor.getString(cursor.getColumnIndexOrThrow(MeetingValues.COLUMN_NAME_TIME)),
+                cursor.getString(cursor.getColumnIndexOrThrow(PassedMeetingValues.COLUMN_NAME_DATE)),
+                cursor.getString(cursor.getColumnIndexOrThrow(PassedMeetingValues.COLUMN_NAME_TIME)),
                 ""));
         meetingListView.setAdapter(new MeetingsAdapter(getActivity(), (BaseActivity) getActivity(),cursor,true));
     }
