d5b91032c43ad8dd51f4a2541baae1fe2f3b9e51
Gerardo Medina <gera.mdlh@gmail.com>
2017-05-03 17:55:41 +0100

    Invitations

--- app/src/main/java/net/gerardomedina/meetandeat/view/activity/InvitationsActivity.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/activity/InvitationsActivity.java
@@ -2,14 +2,33 @@ package net.gerardomedina.meetandeat.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import net.gerardomedina.meetandeat.R;
 
 public class InvitationsActivity extends BaseActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.getItem(0).setEnabled(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        changeToActivity(InvitationsActivity.class);
+        overridePendingTransition(R.anim.fade_in,R.anim.slide_up);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invitations);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.title_invitations);
     }
 }
