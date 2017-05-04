8911dd7a94641666236713d4a6ba38216edd74a4
Gerardo Medina <gera.mdlh@gmail.com>
2017-05-04 15:40:41 +0100

    invitations

--- app/src/main/java/net/gerardomedina/meetandeat/view/activity/InvitationsActivity.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/activity/InvitationsActivity.java
@@ -2,11 +2,19 @@ package net.gerardomedina.meetandeat.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import net.gerardomedina.meetandeat.R;
+import net.gerardomedina.meetandeat.view.adapter.InvitationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InvitationsActivity extends BaseActivity {
 
@@ -28,8 +36,23 @@ public class InvitationsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invitations);
+        setToolbar();
+        setInvitationsList();
+    }
+
+    private void setInvitationsList() {
+        ListView invitationsList = (ListView) findViewById(R.id.invitations);
+
+
+
+        invitationsList.setAdapter(new InvitationAdapter(this,new ArrayList<String>()));
+    }
+
+    private void setToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.title_invitations);
         setSupportActionBar(toolbar);
     }
+
+
 }
---
+++ app/src/main/java/net/gerardomedina/meetandeat/view/adapter/InvitationAdapter.java
@@ -0,0 +1,35 @@
+package net.gerardomedina.meetandeat.view.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import net.gerardomedina.meetandeat.R;
+import net.gerardomedina.meetandeat.view.activity.InvitationsActivity;
+
+import java.util.List;
+
+public class InvitationAdapter extends ArrayAdapter<String> {
+    private InvitationsActivity invitationsActivity;
+
+    public InvitationAdapter(InvitationsActivity invitationsActivity, List<String> invitations) {
+        super(invitationsActivity, 0, invitations);
+        this.invitationsActivity = invitationsActivity;
+    }
+
+    @NonNull
+    @Override
+    public View getView(final int position, View convertView, @NonNull ViewGroup parent) {
+        final String invitationLabel = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        }
+        TextView invitation = (TextView) convertView.findViewById(R.id.text1);
+        invitation.setText(invitationLabel);
+        return convertView;
+    }
+}
