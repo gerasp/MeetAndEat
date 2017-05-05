8391001d53a5d0cda2e66100fac4c1d7c102fad3
Gerardo Medina <gera.mdlh@gmail.com>
2017-05-05 20:24:39 +0100

    Location

--- app/src/main/java/net/gerardomedina/meetandeat/view/activity/LocationActivity.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/activity/LocationActivity.java
@@ -1,7 +1,6 @@
 package net.gerardomedina.meetandeat.view.activity;
 
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,30 +14,24 @@ import net.gerardomedina.meetandeat.model.Meeting;
 public class LocationActivity extends BaseActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
+    private Meeting meeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
-        setToolbar();
+
+        meeting = appCommon.getSelectedMeeting();
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
     }
 
-    private void setToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.location);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        Meeting meeting = appCommon.getSelectedMeeting();
-
         map.addMarker(new MarkerOptions().position(meeting.getLocation()).title(meeting.getTitle()));
         map.moveCamera(CameraUpdateFactory.newLatLng(meeting.getLocation()));
         map.animateCamera(CameraUpdateFactory.zoomTo(15));
