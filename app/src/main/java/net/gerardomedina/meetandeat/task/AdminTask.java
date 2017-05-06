d2f422779fabd9724d074a63bef571ce502db429
Gerardo Medina <gera.mdlh@gmail.com>
2017-05-06 17:55:15 +0100

    Admin

---
+++ app/src/main/java/net/gerardomedina/meetandeat/task/AdminTask.java
@@ -0,0 +1,54 @@
+package net.gerardomedina.meetandeat.task;
+
+import android.util.Log;
+
+import net.gerardomedina.meetandeat.R;
+import net.gerardomedina.meetandeat.view.activity.BaseActivity;
+import net.gerardomedina.meetandeat.view.activity.MainActivity;
+
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AdminTask extends BaseTask {
+
+    private final String parameter;
+    private final int type;
+
+    public AdminTask(BaseActivity activity, int type, String parameter) {
+        this.activity = activity;
+        this.type = type;
+        this.parameter = parameter;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        Map<String,String> parameters = new HashMap<>();
+        parameters.put("user_id",appCommon.getUser().getId()+"");
+        parameters.put("meeting_id",appCommon.getSelectedMeeting().getId()+"");
+        parameters.put("type", type+"");
+        parameters.put("parameter", parameter);
+
+        response = requester.httpRequest("Admin.php", "POST", parameters);
+        return true;
+    }
+
+    @Override
+    protected void onPostExecute(final Boolean success) {
+        super.onPostExecute(success);
+        if (success) {
+            try {
+                switch (response.getInt("code")) {
+                    case 0: activity.showSimpleDialog(R.string.error);
+                            break;
+                    case 2: activity.showSimpleDialog(R.string.done);
+                            if (type == 5) activity.changeToActivityNoBackStack(MainActivity.class);
+                            break;
+                }
+            } catch (JSONException e) {
+                Log.e("JSON Parser", "Error parsing data: " + e.toString());
+            }
+        }
+    }
+}
--- app/src/main/java/net/gerardomedina/meetandeat/view/activity/AdminActivity.java
+++ app/src/main/java/net/gerardomedina/meetandeat/view/activity/AdminActivity.java
@@ -3,12 +3,16 @@ package net.gerardomedina.meetandeat.view.activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import net.gerardomedina.meetandeat.R;
+import net.gerardomedina.meetandeat.model.Meeting;
 import net.gerardomedina.meetandeat.model.Option;
+import net.gerardomedina.meetandeat.task.AdminTask;
 import net.gerardomedina.meetandeat.view.adapter.OptionAdapter;
 
 import java.util.ArrayList;
@@ -16,10 +20,13 @@ import java.util.List;
 
 public class AdminActivity extends BaseActivity {
 
+    private Meeting meeting;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        meeting = appCommon.getSelectedMeeting();
         setupActionBar();
         setupOptionsList();
     }
@@ -37,7 +44,25 @@ public class AdminActivity extends BaseActivity {
         options.add(new Option(getString(R.string.change_title), new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                final EditText input = new EditText(getBaseActivity());
+                input.setText(meeting.getTitle());
+                final AlertDialog alertDialog = new AlertDialog.Builder(getBaseActivity()).create();
+                alertDialog.setMessage(getString(R.string.request_email));
+                alertDialog.setView(input);
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (input.getText().length() > 20) showToast(R.string.error_field_too_long);
+                        else new AdminTask(getBaseActivity(),0,input.getText().toString());
+                    }
+                });
+                alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        alertDialog.dismiss();
+                    }
+                });
+                alertDialog.show();
             }
         }));
         options.add(new Option(getString(R.string.change_location), new View.OnClickListener() {
@@ -67,7 +92,7 @@ public class AdminActivity extends BaseActivity {
         options.add(new Option(getString(R.string.delete_meeting), new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new android.support.v7.app.AlertDialog.Builder(this, R.style.MyAlertDialogStyle)
+                new android.support.v7.app.AlertDialog.Builder(getBaseActivity(), R.style.MyAlertDialogStyle)
                         .setMessage(R.string.are_you_sure)
                         .setNegativeButton(getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
                             @Override
@@ -75,7 +100,7 @@ public class AdminActivity extends BaseActivity {
                         })
                         .setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
                             @Override
-                            public void onClick(DialogInterface dialog, int which) {new AdminTask(7,"").execute();}
+                            public void onClick(DialogInterface dialog, int which) {new AdminTask(getBaseActivity(),5,"").execute();}
                         })
                         .create()
                         .show();
